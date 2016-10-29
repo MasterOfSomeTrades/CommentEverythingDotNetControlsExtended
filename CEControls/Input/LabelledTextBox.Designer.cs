609da881d854490244ade118f227a1db631913cb
MasterOfSomeTrades <ti_grrrrrr@hotmail.com>
2016-10-29 19:01:16 -0400

    Create initial files, and LabelledTextBox control

---
+++ CEControls/Input/LabelledTextBox.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CEControls.Input {
+    partial class LabelledTextBox {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this._textbox = new System.Windows.Forms.TextBox();
+            this._label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _textbox
+            // 
+            this._textbox.Location = new System.Drawing.Point(3, 20);
+            this._textbox.Name = "_textbox";
+            this._textbox.Size = new System.Drawing.Size(340, 20);
+            this._textbox.TabIndex = 0;
+            // 
+            // _label
+            // 
+            this._label.AutoSize = true;
+            this._label.Location = new System.Drawing.Point(3, 4);
+            this._label.Name = "_label";
+            this._label.Size = new System.Drawing.Size(0, 13);
+            this._label.TabIndex = 1;
+            // 
+            // LabelledTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._label);
+            this.Controls.Add(this._textbox);
+            this.Name = "LabelledTextBox";
+            this.Size = new System.Drawing.Size(346, 42);
+            this.Resize += new System.EventHandler(this.LabelledTextBox_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _textbox;
+        private System.Windows.Forms.Label _label;
+    }
+}
---
+++ CEControls/Input/LabelledTextBox.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CEControls.Input {
+    public partial class LabelledTextBox : UserControl {
+        /// <summary>
+        /// Property representing Text of the LabelledTextBox.
+        /// </summary>
+        public override string Text {
+            get {
+                return _textbox.Text;
+            }
+            set {
+                _textbox.Text = value;
+            }
+        }
+        /// <summary>
+        /// Property representing Label of the LabelledTextBox.
+        /// </summary>
+        public string Label {
+            get {
+                return _label.Text;
+            }
+            set {
+                _label.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// No-arg constructor for LabelledTextbox.
+        /// </summary>
+        public LabelledTextBox() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor for LabelledTextbox with Label initialized.
+        /// </summary>
+        /// <param name="label"></param>
+        public LabelledTextBox(string label) {
+            InitializeComponent();
+            Label = label;
+        }
+
+        /// <summary>
+        /// Constructor for LabelledTextbox with Width initialized.
+        /// </summary>
+        /// <param name="width"></param>
+        public LabelledTextBox(int width) {
+            InitializeComponent();
+            Width = width;
+        }
+
+        /// <summary>
+        /// Constructor for LabelledTextbox with Label and Width initialized.
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="width"></param>
+        public LabelledTextBox(string label, int width) {
+            InitializeComponent();
+            Label = label;
+            Width = width;
+        }
+
+        /// <summary>
+        /// Handles event when LabelledTextBox resizes to resize TextBox accordingly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LabelledTextBox_Resize(object sender, EventArgs e) {
+            _textbox.Width = ClientSize.Width - _textbox.Left - 1;
+        }
+    }
+}
