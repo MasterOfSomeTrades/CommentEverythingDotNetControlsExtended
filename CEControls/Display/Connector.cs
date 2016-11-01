1bf2f4f32114fb97d6bc252e149bf078e4f9b8f4
MasterOfSomeTrades <ti_grrrrrr@hotmail.com>
2016-11-01 02:30:55 -0400

    Fix Connector render

--- CEControls/Display/Connector.cs
+++ CEControls/Display/Connector.cs
@@ -5,13 +5,25 @@ using System.Linq;
 using System.Text;
 
 namespace CEControls.Display {
-    class Connector : DisplayGraphic {
+    public class Connector : DisplayGraphic {
         public Point p1 { get; set; }
         public Point p2 { get; set; }
 
         public override void Render() {
             Graphics renderer = CreateGraphics();
 
+            Top = p1.Y;
+            Height = p2.Y - p1.Y;
+            if (p2.Y < Top) {
+                Top = p2.Y;
+                Height = p1.Y - p2.Y;
+            }
+            Left = p1.X;
+            Width = p2.X - p1.X;
+            if (p2.X < Left) {
+                Left = p2.X;
+                Width = p1.X - p2.X;
+            }
             renderer.DrawLine(Pens.Black, p1, p2);
             renderer.Dispose();
         }
